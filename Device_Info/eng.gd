e34e3548b7dbc6a377ed6cdb3abe37de9e693f42
Łucky <luckytkig@gmail.com>
2024-10-26 20:53:22 +0300

    Add files via upload

---
+++ Device_Info/eng.gd
@@ -0,0 +1,21 @@
+extends Label
+
+func _ready():
+	# Получаем основную информацию о устройстве
+	var device_name = OS.get_name()  # Имя операционной системы
+	var device_version = OS.get_version()  # Версия операционной системы
+	var device_cpu_count = OS.get_processor_count()  # Количество процессоров
+	var cpu_info = OS.get_processor_name()  # Имя процессора
+	var is_debug_build = OS.is_debug_build()  # Проверка, является ли сборка отладочной
+
+	# Локализованные строки
+	var info_text = "Device: %s\nVersion: %s\nProcessor Count: %d\nProcessor Name: %s\nDebug Build: %s" % [
+		device_name,
+		device_version,
+		device_cpu_count,
+		cpu_info,
+		str(is_debug_build)
+	]
+	
+	# Устанавливаем текст в лейбл
+	text = info_text
